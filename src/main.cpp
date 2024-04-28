f8c0693caf2a5c28168532f0d4773b94d8ec698d
Antonino Rando <rando.1985084@studenti.uniroma1.it>
2024-04-29 00:53:42 +0200

    refactor: Add CMakeLists

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "state.h"
 #include "settings.h"
 #include <conio.h>
+#include <hiredis.h>
 
 // We use wasd for movement because the _getchr() return twice with arrows keys:
 // https://learn.microsoft.com/en-us/previous-versions/visualstudio/visual-studio-2010/078sfkak(v=vs.100)?redirectedfrom=MSDN
@@ -9,6 +10,13 @@ int main()
 {
     GlobalSettings::load();
     std::cout << "\u001b[" + BG_COL + "m";
+
+    redisContext *c = redisConnect("127.0.0.1", 6379);
+    if (c != nullptr && c->err)
+    {
+        std::cout << "ERROR Trying to connect to redis server";
+        return 1;
+    }
 
     Context game;
     game.transitionTo(new Opening);
