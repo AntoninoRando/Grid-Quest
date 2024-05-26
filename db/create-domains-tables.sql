eec79fc704fe12d3597e512b88f46a9440a97ab1
Antonino Rando <rando.1985084@studenti.uniroma1.it>
2024-05-26 19:27:55 +0200

    feat: Add database schema and tables creation scripts

---
+++ db/create-domains-tables.sql
@@ -0,0 +1,76 @@
+BEGIN TRANSACTION;
+
+-- Remove previous tables and domains
+
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
+
+-- Domains
+
+CREATE DOMAIN cell_cord AS INTEGER CHECK (VALUE >= 0 AND VALUE <= 9);
+CREATE DOMAIN s_str AS VARCHAR(20);
+CREATE DOMAIN m_str AS VARCHAR(100);
+CREATE DOMAIN grid AS VARCHAR(318) CHECK (VALUE ~ '^(\.{0,2}\d+)+$');
+CREATE TYPE cells_operation AS ENUM('Add', 'Sub', 'Mul', 'Div', 'Mod', 'Concat');
+CREATE TYPE cell AS (value INTEGER, x cell_cord, y cell_cord);
+-- ^       = start of the string;
+-- \.{0,2} = between 0 and 2 dots: "", ".", or "..";
+-- \d+     = any number of digits;
+-- (...)+  = match anything that is inside parentheses any number of times;
+-- $       = end of string
+--
+-- We represent a grid from the left bottom corner to the top right corner,
+-- moving from left to right and from bottom to top. Every cell is separated by
+-- a dot. Every row is separated by two dots.
+--
+-- Supposing we have a two-digit number in every cell of a full grid, we need a
+-- 3 string per cell, plus 2 strings per row (except the last), for a total of 
+-- 318. The grid only represent the initial grid, where cells can't have a
+-- higher value than 99. (Remember that a grid is a 10x10.)
+
+
+-- Tables
+
+CREATE TABLE Profile(
+	nickame m_str PRIMARY KEY,
+	creation date NOT NULL
+);
+
+CREATE TABLE Session(
+    player m_str,
+	startstamp TIMESTAMP,
+    endstamp TIMESTAMP NOT NULL,
+    max_wait REAL NOT NULL,
+	avg_wait REAL NOT NULL,
+    PRIMARY KEY (player, startstamp),
+	FOREIGN KEY (player) REFERENCES Profile(nickame),
+    CONSTRAINT start_before_end CHECK (startstamp < endstamp)
+);
+
+CREATE TABLE Quest(
+    id SERIAL PRIMARY KEY,
+	session_player m_str,
+    session_startstamp TIMESTAMP,
+    goal INTEGER NOT NULL,
+    startstamp TIMESTAMP,
+    endstamp TIMESTAMP NOT NULL,
+	result INTEGER,
+    hp INTEGER NOT NULL,
+    grid grid NOT NULL,
+    FOREIGN KEY (session_player, session_startstamp) REFERENCES Session(player, startstamp),
+    UNIQUE (session_player, session_startstamp, startstamp),
+    CONSTRAINT start_before_end CHECK (startstamp < endstamp)
+);
+
+CREATE TABLE Operation(
+    quest INTEGER,
+    ord INTEGER,
+	op_type cells_operation NOT NULL, 
+	primary_cell cell NOT NULL,
+    secondary_cell cell NOT NULL,
+    PRIMARY KEY (quest, ord),
+    FOREIGN KEY (quest) REFERENCES Quest(id),
+    CONSTRAINT non_negative_ord CHECK (ord >= 0)
+);
+
+COMMIT;
